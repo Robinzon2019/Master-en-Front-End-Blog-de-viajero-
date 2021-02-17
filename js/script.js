8ac79b2e317dffa31252424c3ab5f67934e50d08
Robinzon2019 <el.robin04@hotmail.com>
2021-02-16 23:12:48 -0400

    Aplicando efectos parallax al banner

--- js/script.js
+++ js/script.js
@@ -12,3 +12,25 @@ $(".fade-slider").jdSlider({
 	isCursor: false,
 	speed: 3000
 });
+
+
+var alturaBanner = $(".fade-slider").height();
+// console.log("alturaBanner", alturaBanner);
+
+$(".bannerEstatico").css({"height":alturaBanner+"px"});
+
+$(window).scroll(function(){
+	var posY = window.pageYOffset;
+	console.log("posY", posY);
+
+	if(posY > alturaBanner){
+		$("header").css({"background":"white"});
+		$("header .logotipo").css({"filter":"invert(100%)"});
+		$(".fa-search, .fa-bars").css({"color":"black"});
+	}
+	else{
+		$("header").css({"background":"rgba(0,0,0,.5)"});
+		$("header .logotipo").css({"filter":"invert(0%)"});
+		$(".fa-search, .fa-bars").css({"color":"white"});
+	}
+});
